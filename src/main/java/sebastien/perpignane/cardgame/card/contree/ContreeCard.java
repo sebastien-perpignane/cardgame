90ca800b01866294ca05e5da60d832807abcef52
sebastien-perpignane <63523775+sebastien-perpignane@users.noreply.github.com>
2022-12-20 00:06:00 +0000

    code cleaning : useless interface implem for ContreeCard

--- src/main/java/sebastien/perpignane/cardgame/card/contree/ContreeCard.java
+++ src/main/java/sebastien/perpignane/cardgame/card/contree/ContreeCard.java
@@ -1,6 +1,5 @@
 package sebastien.perpignane.cardgame.card.contree;
 
-import sebastien.perpignane.cardgame.card.Card;
 import sebastien.perpignane.cardgame.card.CardRank;
 import sebastien.perpignane.cardgame.card.CardSuit;
 import sebastien.perpignane.cardgame.card.ClassicalCard;
@@ -9,7 +8,7 @@ import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-public class ContreeCard implements ValuableCard, Card {
+public class ContreeCard implements ValuableCard {
 
     private final ClassicalCard card;
 
