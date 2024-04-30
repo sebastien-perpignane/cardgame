1c2b68e49e7d72dc95a14a1f3b4eacd8d53ba489
Sébastien Perpignane <sebastien.perpignane@gmail.com>
2024-05-01 01:46:59 +0200

    feat/preparing support for all trumps and no trumps deals

---
+++ src/test/java/sebastien/perpignane/cardgame/card/contree/ContreeCardTest.java
@@ -0,0 +1,42 @@
+package sebastien.perpignane.cardgame.card.contree;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import sebastien.perpignane.cardgame.card.CardSet;
+import sebastien.perpignane.cardgame.card.CardSuit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ContreeCardTest {
+
+    @ParameterizedTest
+    @EnumSource(names = {"DIAMONDS", "HEARTS"})
+    void testOf(CardSuit trumpSuit) {
+
+        var result = ContreeCard.of(trumpSuit, CardSet.GAME_32.getGameCards());
+
+        assertThat(result.stream().filter(ContreeCard::isTrump).toList().size()).isEqualTo(8);
+
+    }
+
+    @Test
+    void ofAllTrumps() {
+
+        var result = ContreeCard.ofAllTrumps(CardSet.GAME_32.getGameCards());
+
+        assertThat(result.stream()).allMatch(ContreeCard::isTrump);
+        assertThat(result).hasSize(32);
+
+    }
+
+    @Test
+    void ofNoTrumps() {
+
+        var result = ContreeCard.ofNoTrumps(CardSet.GAME_32.getGameCards());
+
+        assertThat(result.stream()).noneMatch(ContreeCard::isTrump);
+        assertThat(result).hasSize(32);
+
+    }
+}
